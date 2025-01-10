8b49b81656babc9dfe2b30dfd046f90d05700186
Leandrho <leandro.coder01@gmail.com>
2025-01-10 16:28:44 -0300

    add schemas

---
+++ src/schema/weather-schema.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+
+export const WeatherSchema = z.object({
+    name: z.string(),
+    timezone: z.number(),
+    main: z.object({
+        temp: z.number(),
+        feels_like: z.number(),
+        temp_min: z.number(),
+        temp_max: z.number(),
+        pressure: z.number(),
+        humidity: z.number(),
+    }),
+    visibility: z.number(),
+    wind: z.object({
+        speed: z.number(),
+        deg: z.number(),
+    }),
+    sys: z.object({
+        country: z.string(),
+        sunrise: z.number(),
+        sunset: z.number(),
+    })
+});
+
+export const WeatherCoordsSchema = z.object({
+    country: z.string(),
+    name: z.string(),
+    lat: z.number(),
+    lon: z.number(),
+    state: z.string()
+})
