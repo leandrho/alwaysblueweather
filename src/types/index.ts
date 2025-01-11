b8df97a775f2598f21cf727d616dd89c5a2476c4
Leandrho <leandro.coder01@gmail.com>
2025-01-11 18:26:10 -0300

    rm WeatherSearch type

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,3 @@ import { WeatherSchema, WeatherCoordsSchema } from '../schema/weather-schema';
 
 export type WeatherInfo = z.infer<typeof WeatherSchema>;   
 export type WeatherCoords = z.infer<typeof WeatherCoordsSchema>;
-export type WeatherSearch = {
-    name: string, 
-    country_code: string,
-}
