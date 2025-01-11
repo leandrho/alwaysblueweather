b44bc7abb67247dac5ace0dfe44fd7d96db4046e
Leandrho <leandro.coder01@gmail.com>
2025-01-11 18:24:52 -0300

    using context

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { WeatherProvider } from './context/WeatherContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WeatherProvider>
+      <App />
+    </WeatherProvider>
   </StrictMode>,
 )
