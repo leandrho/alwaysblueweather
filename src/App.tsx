7f11b08f9ef35c0d03fd7edceadbe0dc8378f1ba
Leandrho <leandro.coder01@gmail.com>
2025-01-12 20:54:01 -0300

    using components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { WeatherHeader } from './components/WeatherHeader';
+import { WeatherHero } from './components/WeatherHero';
 function App() {
   
   return (
-    <>
-      <h1 className="text-4xl">AlwaysBlue Weather</h1>
-      <img src="/logoa-120.png" alt="" />
-    </>
+    <div className='flex flex-col h-screen'>
+      <WeatherHeader/>
+      <main className='flex flex-grow p-8'> {/*bg-hero-summer bg-cover bg-center bg-no-repeat*/}
+        <WeatherHero/>
+        {/* <div className='w-96 h-96 bg-red-600'></div> */}
+      </main>
+    </div>
   )
 }
 
-export default App
+export default App;
