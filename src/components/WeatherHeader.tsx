e351ede88dcdb079c91a5e7285928adbf7db61c7
Leandrho <leandro.coder01@gmail.com>
2025-01-12 20:51:54 -0300

    add Header component

---
+++ src/components/WeatherHeader.tsx
@@ -0,0 +1,13 @@
+
+export const WeatherHeader = () => {
+  return (
+    <header className="bg-blue-950 text-white">
+        <div className="flex items-center gap-4 p-4 bg">
+            <figure className="max-w-12">
+                <img src="/logoa-120.png" alt="" />
+            </figure>
+            <h1 className="text-base text-white font-bold">AlwaysBlue Weather</h1>
+        </div>
+    </header>
+  )
+}
