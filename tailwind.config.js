335b191e88ec1f9dca984682ec5665c4cc3a0e45
Leandrho <leandro.coder01@gmail.com>
2025-01-12 20:54:57 -0300

    add bg image to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-summer': "url('/bg-summer.jpg')",
+      },
+    },
   },
   plugins: [],
 }
