56f324c61b2935a93bfa3bd9985a65ce40df8e9d
Leandrho <leandro.coder01@gmail.com>
2025-01-13 20:29:13 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { WeatherHeader } from './components/WeatherHeader';
 import { WeatherHero } from './components/WeatherHero';
+import { WeatherPanel } from './components/WeatherPanel';
 function App() {
   
   return (
@@ -7,6 +8,7 @@ function App() {
       <WeatherHeader/>
       <main className='flex flex-grow p-8'> {/*bg-hero-summer bg-cover bg-center bg-no-repeat*/}
         <WeatherHero/>
+        <WeatherPanel />
         {/* <div className='w-96 h-96 bg-red-600'></div> */}
       </main>
     </div>
