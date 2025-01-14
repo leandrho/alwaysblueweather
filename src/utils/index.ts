eb96066d0d32be845495afabbd5fbdfef251f845
Leandrho <leandro.coder01@gmail.com>
2025-01-14 17:17:22 -0300

    add helper getIcon function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,24 @@
+import { SimpleIcon } from '../types/index';
+
 export const kelvinToCelsius = (temp: number):number => {
     return Math.round(Number((temp-273.15).toFixed(1)) * 2) / 2;
+}
+
+export const getIcon = (icon :SimpleIcon) : string => {
+    switch (icon) {
+        case SimpleIcon.Cloudy:
+            return '/cloudy.svg';
+        case SimpleIcon.Temp:
+            return '/temp.svg';
+        case SimpleIcon.TempMax:
+            return '/tempmax.svg';
+        case SimpleIcon.TempMin:
+            return '/tempmin.svg';
+        case SimpleIcon.Windy:
+            return '/wind.svg';
+        case SimpleIcon.Humadity:
+            return '/humadity.svg';
+        default:
+            return '';
+    }
 }
