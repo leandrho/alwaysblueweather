b04e5a1eeb9d4adbbce63a311a3a1c3f601f6cd3
Leandrho <leandro.coder01@gmail.com>
2025-01-14 17:17:59 -0300

    add SimpleIcon enum ..

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,12 @@ import { WeatherSchema, WeatherCoordsSchema } from '../schema/weather-schema';
 
 export type WeatherInfo = z.infer<typeof WeatherSchema>;   
 export type WeatherCoords = z.infer<typeof WeatherCoordsSchema>;
+
+export enum SimpleIcon {
+    TempMax,
+    TempMin,
+    Temp,
+    Windy,
+    Humadity,
+    Cloudy
+}
