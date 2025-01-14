93469aff95678b356832d9b0c0864166b7207bee
Leandrho <leandro.coder01@gmail.com>
2025-01-14 15:41:42 -0300

    update search

--- src/components/WeatherSearch.tsx
+++ src/components/WeatherSearch.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useRef } from "react";
+import { useWeather } from "../hooks/useWeather";
+import { getLatLong } from "../services";
+
 
 
 export const WeatherSearch = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const {searchWeather, searchWeatherByLatLong} = useWeather();
+
+      useEffect(() => {
+          const init = async () => {
+            const {lat, long} = await getLatLong();
+            await searchWeatherByLatLong(lat, long);
+          }
+          init();
+      }, [])
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('SEARCH');
+        if(inputRef.current){
+            let value = inputRef.current.value.trim();
+            if(value)
+                searchWeather(value, 'AR');
+        }
     }
     return (
         <form onSubmit={handleSubmit} className="p-4 flex gap-1">
-            <input type="text" className="p-4 w-full rounded-md" aria-label="search text"/>
+            <input ref={inputRef} type="text" className="p-4 w-full rounded-md" aria-label="search text"/>
             <button type="submit" className="font-bold bg-blue-950 text-white p-4 rounded-md">Buscar</button>
         </form>
     )
