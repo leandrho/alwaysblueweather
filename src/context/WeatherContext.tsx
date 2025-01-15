a53483f5643d3d852055c10dacd0ed61f8ad57be
Leandrho <leandro.coder01@gmail.com>
2025-01-15 13:54:43 -0300

    cleaning provider

--- src/context/WeatherContext.tsx
+++ src/context/WeatherContext.tsx
@@ -1,88 +1,56 @@
-import { createContext, ReactElement, useState } from "react";
+import { createContext, ReactElement } from "react";
 import { WeatherInfo } from '../types/index';
-import { getWeatherInfo, getWeatherInfoByLatLong } from "../services";
-import { kelvinToCelsius } from "../utils";
+import { useWeatherState } from "../hooks/useWeatherState";
 
-type WeatherContextType = {
-    weatherInfo: WeatherInfo,
-    searchWeather: (name: string, countryCode: string) => Promise<void>
-    searchWeatherByLatLong: (lat: number, long: number) => Promise<void>
-    errorMsg: string
-    loading: boolean
-    cleanError :()=>void
+export type WeatherContextType = {
+    weatherInfo: WeatherInfo[];
+    searchWeather: (name: string, countryCode: string) => Promise<void>;
+    searchWeatherByLatLong: (lat: number, long: number) => Promise<void>;
+    errorMsg: string;
+    loading: boolean;
+    cleanError: () => void;
+    selectedWeatherID: number;
+    setCurrentWeatherID: (id: number) => void;
+    removeWeatherInfo: (id: number) => void;
 }
 export const WeatherContext = createContext({} as WeatherContextType);
 
-export const initialState :WeatherInfo = {
-    name: '',
-    timezone: 0,
-    main:{
-        temp: 0,
-        feels_like: 0,
-        temp_min: 0,
-        temp_max: 0,
-        pressure: 0,
-        humidity: 0,
-    },
-    visibility: 0,
-    wind:{
-        speed: 0,
-        deg: 0,
-    },
-    sys: {
-        country: '',
-        sunrise: 0,
-        sunset: 0,
-    },
-    clouds: {
-        all: 0
-    }
-};
+// export const initialState :WeatherInfo = {
+//     id: 0,
+//     dt: 0,
+//     name: '',
+//     timezone: 0,
+//     main:{
+//         temp: 0,
+//         feels_like: 0,
+//         temp_min: 0,
+//         temp_max: 0,
+//         pressure: 0,
+//         humidity: 0,
+//     },
+//     visibility: 0,
+//     wind:{
+//         speed: 0,
+//         deg: 0,
+//     },
+//     sys: {
+//         country: '',
+//         sunrise: 0,
+//         sunset: 0,
+//     },
+//     clouds: {
+//         all: 0
+//     }
+// };
 
 export type WeatherProviderProp = {
     children: ReactElement
 }
 export const WeatherProvider = ( { children }: WeatherProviderProp ) => {
-
-  const [weatherInfo, setWeatherInfo] = useState<WeatherInfo>(initialState)
-  const [errorMsg, setErrorMsg] = useState('')
-  const [loading, setLoading] = useState(false)
-
-  const searchWeather = async (name: string, countryCode: string) : Promise<void> =>{
-
-    setLoading(true);
-    const newWeatherInfo : WeatherInfo = await getWeatherInfo(name, countryCode);
-    console.log('WINFO: ', newWeatherInfo)
-    if(newWeatherInfo.name){
-        setWeatherInfo({...newWeatherInfo, main: {...newWeatherInfo.main, temp: kelvinToCelsius(newWeatherInfo.main.temp), temp_max: kelvinToCelsius(newWeatherInfo.main.temp_max), temp_min: kelvinToCelsius(newWeatherInfo.main.temp_min)}});
-        setErrorMsg('');
-    }
-    else{
-        setWeatherInfo(initialState);
-        setErrorMsg(`No se puede encontrar la ciudad : ${name}`);
-    }
-    setLoading(false);
-  }
-  const searchWeatherByLatLong = async (lat: number, long: number) : Promise<void> =>{
-
-    setLoading(true);
-    const newWeatherInfo : WeatherInfo = await getWeatherInfoByLatLong(lat, long);
-
-    if(newWeatherInfo.name){
-        setWeatherInfo({...newWeatherInfo, main: {...newWeatherInfo.main, temp: kelvinToCelsius(newWeatherInfo.main.temp), temp_max: kelvinToCelsius(newWeatherInfo.main.temp_max), temp_min: kelvinToCelsius(newWeatherInfo.main.temp_min)}});
-        setErrorMsg('');
-    }
-    else{
-        setWeatherInfo(initialState);
-        setErrorMsg("No se puede encontrar la ciudad ingresada!!");
-    }
-    setLoading(false);
-  }
-  const cleanError = () => {
-    setErrorMsg('');
-  }
+   const useweatherstate = useWeatherState();
+ 
   return (
-    <WeatherContext.Provider value={{ weatherInfo, searchWeather, searchWeatherByLatLong, errorMsg, loading, cleanError }}>
+    <WeatherContext.Provider value={ useweatherstate }>
         { children }
     </WeatherContext.Provider>
   )
---
+++ src/hooks/useWeatherState.ts
@@ -0,0 +1,74 @@
+import { useCallback, useEffect, useState } from 'react';
+import { WeatherInfo } from '../types/index';
+import { getWeatherInfo, getWeatherInfoByLatLong } from '../services';
+import { kelvinToCelsius } from '../utils';
+
+export const useWeatherState = () => {
+    
+    const [weatherInfo, setWeatherInfo] = useState<WeatherInfo[]>([]);
+    const [selectedWeatherID, setSelectedWeatherID] = useState<number>(0);
+    const [errorMsg, setErrorMsg] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+  
+    useEffect(() => {
+      if(weatherInfo.length > 0)
+        setSelectedWeatherID(weatherInfo[weatherInfo.length-1].id);
+
+    }, [weatherInfo])
+    
+
+    const searchWeather = useCallback( async (name: string, countryCode: string) : Promise<void> =>{
+  
+        if(weatherInfo.some((w)=>w.name.toLowerCase()==name.toLowerCase()))
+            return;
+        setLoading(true);
+        try {
+            const newWeatherInfo : WeatherInfo = await getWeatherInfo(name, countryCode);
+            const n = {...newWeatherInfo, id: Date.now(), main: {...newWeatherInfo.main, temp: kelvinToCelsius(newWeatherInfo.main.temp), temp_max: kelvinToCelsius(newWeatherInfo.main.temp_max), temp_min: kelvinToCelsius(newWeatherInfo.main.temp_min)}};
+            setWeatherInfo((prev)=>[...prev, n]);
+            setErrorMsg('');
+           
+        } catch (error) {
+            setErrorMsg(`No se puede encontrar la ciudad : ${name}`);
+        }
+        finally{
+            setLoading(false);
+        }
+    }, []);
+    const searchWeatherByLatLong = useCallback( async (lat: number, long: number) : Promise<void> =>{
+  
+        setLoading(true);
+        try {
+            const newWeatherInfo : WeatherInfo = await getWeatherInfoByLatLong(lat, long);
+            const n = {...newWeatherInfo, id: Date.now(), main: {...newWeatherInfo.main, temp: kelvinToCelsius(newWeatherInfo.main.temp), temp_max: kelvinToCelsius(newWeatherInfo.main.temp_max), temp_min: kelvinToCelsius(newWeatherInfo.main.temp_min)}};
+            setWeatherInfo([n]);
+            setErrorMsg('');
+           
+        } catch (error) {
+            setErrorMsg(`No se puede encontrar la ciudad actual`);
+        }
+        finally{
+            setLoading(false);
+        }
+    }, [])
+    const cleanError = useCallback( () => {
+        setErrorMsg('');
+    },[]);
+    const setCurrentWeatherID = useCallback( (id: number):void=>{
+        setSelectedWeatherID(id);
+    }, [])
+    const removeWeatherInfo = useCallback( (id: number):void => {
+        setWeatherInfo((prev)=>prev.filter(info => info.id!== id));
+    }, [])
+    return {
+        weatherInfo,
+        selectedWeatherID,
+        errorMsg,
+        loading,
+        searchWeather,
+        searchWeatherByLatLong,
+        cleanError,
+        setCurrentWeatherID,
+        removeWeatherInfo
+    };
+}
