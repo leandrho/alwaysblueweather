3959abc6a8a81497fd861c9f616286b12a2da8e9
Leandrho <leandro.coder01@gmail.com>
2025-01-15 13:54:02 -0300

    add widgetlist

---
+++ src/components/WeatherWidgetList.tsx
@@ -0,0 +1,31 @@
+import { useWeather } from "../hooks/useWeather";
+import { WeatherWidget } from "./WeatherWidget";
+
+export const WeatherWidgetList = () => {
+    const {weatherInfo, selectedWeatherID, setCurrentWeatherID, removeWeatherInfo} = useWeather();
+    const setActive = (id: number):void => {
+        setCurrentWeatherID(id);
+    }
+    const removeWidget = (id: number) :void => {
+        console.log('REMOVING..: ', id)
+        removeWeatherInfo(id);
+    }
+    return (
+        <div className="flex flex-col gap-2">
+            {
+                weatherInfo.map( (winfo)=>(
+                    <WeatherWidget
+                        key={winfo.id} 
+                        temp={winfo.main.temp} 
+                        location={winfo.name} 
+                        id={winfo.id} 
+                        date={winfo.dt} 
+                        active={selectedWeatherID===winfo.id}
+                        setActive={setActive}
+                        removeWidget={removeWidget}
+                    />
+                ) )
+            }
+        </div>
+    )
+}
