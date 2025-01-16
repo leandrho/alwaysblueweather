713beb68c21d48b4069ffa8da250a4d8f5c46f68
Leandrho <leandro.coder01@gmail.com>
2025-01-16 09:46:36 -0300

    using bg winter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+
 import { WeatherHeader } from './components/WeatherHeader';
 import { WeatherHero } from './components/WeatherHero';
 import { WeatherPanel } from './components/WeatherPanel';
 import { useWeather } from './hooks/useWeather';
 function App() {
-  const { errorMsg, cleanError } = useWeather();
+  const { errorMsg, cleanError, weatherInfo, selectedWeatherID } = useWeather();
   return (
     <>
     <div className='flex flex-col h-screen bg-cyan-50'>
       <WeatherHeader />
-      <main className='flex flex-grow p-8 bg-hero-summer bg-cover bg-center bg-no-repeat'> {/*bg-hero-summer bg-cover bg-center bg-no-repeat*/}
-        <WeatherHero/>
-        <WeatherPanel />
+      <main className={'flex flex-grow items-center justify-center bg-cover bg-center bg-no-repeat transition-all duration-700 flex-col-reverse gap-8 lg:flex-row lg:items-start '+`${weatherInfo.find((w)=> w.id === selectedWeatherID)!?.main.temp < 15 ? ' bg-hero-winter ':' bg-hero-summer '}`}> {/*bg-hero-summer bg-cover bg-center bg-no-repeat*/}
+        <div className='max-w-[1640px] flex flex-grow items-center p-8 flex-col-reverse gap-8 lg:flex-row lg:items-start'>
+          <WeatherHero/>
+          <WeatherPanel />
+        </div>
         {/* <div className='w-96 h-96 bg-red-600'></div> */}
       </main>
     </div>
